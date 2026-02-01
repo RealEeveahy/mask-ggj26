01cf45970cca421b6ef6a420abc617683950771b
BreeLettusTomato <golddustgeoscience@gmail.com>
2026-02-01 12:13:55 +0800

    Bree Script Update 12
    Work on making Notes and luteBehaviour better.

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -10,6 +10,7 @@ public class Note : MonoBehaviour
     public LuteBehaviour luteBehaviour = null;
     public float noteSpeed = 1f;
     SpriteRenderer renderer = null;
+    public float fretBoardWidth = 1.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,14 +26,22 @@ public class Note : MonoBehaviour
     public void SetNotePosition()
     {
         int randomn = Random.Range(1, 4);
-        Vector3 newVector = new Vector3(randomn, 0, 0);
+        Vector3 newVector = new Vector3(randomn* fretBoardWidth, 0, 0);
         transform.position = creatorObject.transform.position + newVector;
 
     }
-    public void PlayNote()
+    public void PlayNote(bool isSuccessful)
     {
         Debug.Log("FallingObject.PlayNote");
-        // Call Audio manager to play the note.
+        if (isSuccessful)
+        {
+            // Call Audio manager to play the note.
+        }
+        else
+        {
+            // Play twang sound here.
+        }
+
     }
     public IEnumerator RespawnNote(float timing)
     {
@@ -47,23 +56,23 @@ public class Note : MonoBehaviour
     {
         if (collision != null)
         {
-            if (collision.tag == this.tag) { return; }
-            ChangeNoteTransparency(0.5f);
-
+            if (collision.tag == this.tag) { return; } // Ignore other notes.
+            if (collision.tag == "Player" && busy == false ) // Player successfully hit note.
+            {
+                PlayNote(true);
+                luteBehaviour.AddNoteToQueue(this);
+                return;
+            }
+            ChangeNoteTransparency(0.5f); // Note hit the ground...
             luteBehaviour.AddNoteToQueue(this);
             GlobalManagement.instance.DecreaseSanity(sanityCost);
-            if (busy == false)
-            {
-                // Play pin dropping audio.
-                int randomn2 = Random.Range(1, 4);
-                StartCoroutine(RespawnNote(randomn2));
-
-            }
+            PlayNote(false);
         }
     }
     public void ChangeNoteTransparency(float value)
     {
         Color colour = renderer.color;
         colour.a = value;
+        renderer.color = colour;
     }
 }
